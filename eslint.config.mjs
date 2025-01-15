932deb26bc264a06302e698825512df765d1814e
jong-k <fully.charged07@gmail.com>
2025-01-15 10:30:16 +0900

    feat: prettier 설치

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended",
+  ),
+  ...compat.plugins("prettier"),
 ];
 
 export default eslintConfig;
