c396b50bfd45faf97a2b29af18f4a452f3744257
jong-k <fully.charged07@gmail.com>
2025-01-22 07:22:31 +0900

    feat: top banner 완성

--- src/components/home/TopBanner.tsx
+++ src/components/home/TopBanner.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 export default function TopBanner() {
   return (
-    <section className="h-[53px] w-full bg-top-banner">홈페이지서만 보이는 배너</section>
+    <section className="flex h-[53px] w-full items-center justify-center bg-top-banner">
+      <div className="text-lg">즉시 인도 가능한 Model Y&nbsp;</div>
+      <p className="text-lg">4739만원 부터</p>
+      <Link href="/" className="ml-4 rounded bg-black px-4 py-1 text-sm text-[#e2e3e3]">
+        주문하기
+      </Link>
+    </section>
   );
 }
