6ff74d6937859dc37bdc6b2ec1da0ca30badfe5b
jong-k <fully.charged07@gmail.com>
2025-01-22 07:35:30 +0900

    feat: top banner height 상수 분리

--- src/components/home/TopBanner.tsx
+++ src/components/home/TopBanner.tsx
@@ -1,8 +1,14 @@
 import Link from "next/link";
+import { TOP_BANNER_HEIGHT } from "@/constants/layout";
 
 export default function TopBanner() {
   return (
-    <section className="flex h-[53px] w-full items-center justify-center bg-top-banner">
+    <section
+      className="flex w-full items-center justify-center bg-top-banner"
+      style={{
+        height: TOP_BANNER_HEIGHT,
+      }}
+    >
       <div className="text-lg">즉시 인도 가능한 Model Y&nbsp;</div>
       <p className="text-lg">4739만원 부터</p>
       <Link href="/" className="ml-4 rounded bg-black px-4 py-1 text-sm text-[#e2e3e3]">
---
+++ src/constants/layout.ts
@@ -0,0 +1,1 @@
+export const TOP_BANNER_HEIGHT = 53;
