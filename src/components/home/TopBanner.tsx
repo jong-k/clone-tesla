2816fa19e4867fe6ad3a5492b6d3d920f9986589
jong-k <fully.charged07@gmail.com>
2025-01-24 07:45:44 +0900

    feat: top banner 반응형 구현

--- src/components/home/TopBanner.tsx
+++ src/components/home/TopBanner.tsx
@@ -1,19 +1,24 @@
 import Link from "next/link";
-import { TOP_BANNER_HEIGHT } from "@/constants/layout";
 
 export default function TopBanner() {
   return (
-    <section
-      className="flex w-full items-center justify-center bg-top-banner"
-      style={{
-        height: TOP_BANNER_HEIGHT,
-      }}
-    >
-      <div className="text-lg">즉시 인도 가능한 Model Y&nbsp;</div>
-      <p className="text-lg">4739만원 부터</p>
-      <Link href="/" className="ml-4 rounded bg-black px-4 py-1 text-sm text-[#e2e3e3]">
-        주문하기
-      </Link>
+    <section className="flex h-[72px] w-full flex-col items-center justify-center bg-top-banner tablet:h-[53px] tablet:flex-row">
+      <div className="text-sm font-semibold">즉시 인도 가능한 Model Y&nbsp;</div>
+      <div className="hidden items-center justify-center tablet:flex">
+        <p>4739만원 부터</p>
+        <Link href="/" className="ml-4 rounded bg-black px-4 py-1 text-sm text-[#e2e3e3]">
+          주문하기
+        </Link>
+      </div>
+      <div className="flex items-center justify-center gap-1 text-sm font-normal tablet:hidden">
+        <div>4739만원 부터</div>
+        <Link
+          href="/"
+          className="underline underline-offset-[3.25px] transition duration-300 hover:text-white"
+        >
+          주문하기
+        </Link>
+      </div>
     </section>
   );
 }
--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -1,4 +1,5 @@
 export const TOP_BANNER_HEIGHT = 53;
+export const TOP_BANNER_HEIGHT_MOBILE = 72;
 
 export const BREAKPOINT = {
   tablet: 600,
