174e46f2d557da67e873001b559b99706f1a4560
jong-k <fully.charged07@gmail.com>
2025-05-24 13:19:20 +0900

    feat: router 추가

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import apiRouter from "./routes/api";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+app.use("/api", apiRouter);
 
 app.get("/", (req, res) => {
   res.send("Top Page");
