15369f0a5edd2d98dd1889c36c1f8f01ee135f2b
jong-k <fully.charged07@gmail.com>
2025-05-25 00:47:43 +0900

    feat: post 요청 라우터 추가

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,16 @@
-import { Router } from "express";
+import express, { Router } from "express";
 
 const router = Router();
+router.use(express.json());
 
 router.get("/", (req, res) => {
   res.send({ message: "Hello from /api route" });
+});
+
+router.post("/", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  res.end();
 });
 
 export default router;
